22c7bd28a3617eaf0650bd19ebf9d8f1bfa74c6e
Timo Reichl <thetredev@gmail.com>
2025-05-03 00:41:58 +0200

    initial commit based on cxx demo project
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

---
+++ build.rs
@@ -0,0 +1,11 @@
+fn main() {
+    cxx_build::bridge("src/main.rs")
+        .file("src/srcds-rs/blobstore.cc")
+        .include("include")
+        .std("c++17")
+        .compile("cxxbridge-demo");
+
+    println!("cargo:rerun-if-changed=src/main.rs");
+    println!("cargo:rerun-if-changed=src/blobstore.cc");
+    println!("cargo:rerun-if-changed=include/blobstore.h");
+}
